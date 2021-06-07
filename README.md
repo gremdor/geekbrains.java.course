d6dd6063bce2a1ff6100ce6422d90abbdbe66108
Gremdor <marador@yandex.ru>
2021-06-07 20:59:19 +0300

    Lesson 3. Collections hometask is developed in package \src\main\java\Task3

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # geekbrains.java.course
+20210607: Added Task3 (Collections)
 20210602: Added Task2 (Exceptions)
---
+++ src/main/java/Task3/PhoneBook.java
@@ -0,0 +1,46 @@
+package Task3;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class PhoneBook {
+    private HashMap<String, ArrayList<String>> phoneBook;// = new HashMap<>();
+
+    public PhoneBook() {
+        this.phoneBook = new HashMap<>();
+    }
+
+    void add(String name, String phoneNum) {
+        ArrayList<String> phones;
+        if (this.phoneBook.containsKey(name)) {
+            phones = this.phoneBook.get(name);
+        } else {
+            phones = new ArrayList<>();
+        }
+
+        phones.add(clrPhone(phoneNum));
+        this.phoneBook.put(name, phones);
+    }
+
+    public String get(String name) {
+        String result = "";
+        if (phoneBook.isEmpty())
+            return result;
+
+        result = phoneBook.get(name).toString();
+        return result;
+    }
+
+    private String clrPhone(String phoneNum) {
+        return phoneNum.replaceAll("\\D","");
+    }
+
+    public void printPhoneBook (){
+        System.out.println("PhoneBook has " + phoneBook.size() + " reords:");
+        for (Map.Entry<String, ArrayList<String>> o : this.phoneBook.entrySet()) {
+            System.out.printf("%s: %s\n", o.getKey(), o.getValue().toString());
+        }
+
+    };
+}
